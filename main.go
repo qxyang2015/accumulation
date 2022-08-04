8d31de44305de845cc1dad9e910d17d8a0f6ee93
qingxiangyang <Wei@100618>
2022-08-04 14:25:35 +0800

    1

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/qxyang2015/accumulation/demo/websocket"
 )
 
 func main() {
-	fmt.Println("start")
-	port := ":9010"
-	path := "/websocket"
-	websocket.SocketServerRun(port, path)
-	fmt.Println("end")
+	fmt.Println("done")
 }
